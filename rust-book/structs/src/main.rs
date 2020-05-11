0c6e65cc21146f0c2f9f69bbe303bc1657f2b512
Jake Scaife <jakescaife@pm.me>
2020-05-11 06:43:43 +0100

    Add examples of structs

---
+++ rust-book/structs/src/main.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+struct UnitStruct;
+
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = build_user("anemail@example.com".to_string(), "ausername".to_string());
+
+    let user3 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
