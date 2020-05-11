59faa131697bad1cfdb2067d07c03357568df11d
Jake Scaife <jakescaife@pm.me>
2020-05-11 06:50:52 +0100

    Add examples of struct methods

--- rust-book/structs/src/main.rs
+++ rust-book/structs/src/main.rs
@@ -13,6 +13,29 @@ struct Point(i32, i32, i32);
 
 struct UnitStruct;
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Self) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
@@ -33,6 +56,25 @@ fn main() {
 
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+    let square = Rectangle::square(5);
 }
 
 fn build_user(email: String, username: String) -> User {
