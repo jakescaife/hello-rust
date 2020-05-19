ef5c9a3e99d423e5eacd2fdbbf2abd3ee1729c54
Jake Scaife <jakescaife@pm.me>
2020-05-19 05:33:24 +0100

    Add examples of threads

---
+++ rust-book/threads/src/main.rs
@@ -0,0 +1,25 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+    let anotherhandle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    anotherhandle.join().unwrap();
+}
