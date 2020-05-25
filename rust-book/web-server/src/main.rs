f5987bda8f05a9eaf8a91052ddff12edce2aafb2
Jake Scaife <jakescaife@pm.me>
2020-05-25 06:19:03 +0100

    Provide an HTML response to valid requests

--- rust-book/web-server/src/main.rs
+++ rust-book/web-server/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(clippy::unused_io_amount)]
 
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -17,7 +18,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("html/hello.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
