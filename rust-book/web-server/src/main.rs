7f0ff1ef5ed79f0a9463dbfa5fd5aa7077f3bf70
Jake Scaife <jakescaife@pm.me>
2020-05-25 06:04:32 +0100

    Provide a response to HTTP requests

--- rust-book/web-server/src/main.rs
+++ rust-book/web-server/src/main.rs
@@ -1,10 +1,23 @@
+#![allow(clippy::unused_io_amount)]
+
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
-        println!("Conntection established!");
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
