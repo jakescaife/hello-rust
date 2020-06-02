d744638feba970d7a6407390afe7ec2d99b0db51
Jake Scaife <jakescaife@pm.me>
2020-06-02 06:14:27 +0100

    Improve linked list by replacing link with option

---
+++ rust-lists/src/second.rs
@@ -0,0 +1,74 @@
+pub struct List {
+    head: Link,
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
+    }
+}
+
+impl Default for List {
+    fn default() -> Self {
+        List { head: None }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut current_link = self.head.take();
+        while let Some(mut boxed_node) = current_link {
+            current_link = boxed_node.next.take();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
