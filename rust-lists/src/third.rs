43edc92e53a7b4ff03f78c2e17e2b34515d2044a
Jake Scaife <jakescaife@pm.me>
2020-06-04 06:06:51 +0100

    Setup basic structure of immutable list

---
+++ rust-lists/src/third.rs
@@ -0,0 +1,43 @@
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn append(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        List { head: None }
+    }
+}
