dc3eb055290b3680156b62ebb1599ea9a7626f57
Jake Scaife <jakescaife@pm.me>
2020-06-08 05:28:03 +0100

    Setup structure of deque

---
+++ rust-lists/src/fourth.rs
@@ -0,0 +1,42 @@
+#![allow(dead_code)]
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+}
