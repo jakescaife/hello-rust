d1f072cfc514a82d5963dd635e9be8f2f90875be
Jake Scaife <jakescaife@pm.me>
2020-06-08 05:45:35 +0100

    Add push and pop functionality for the front of deque

--- rust-lists/src/fourth.rs
+++ rust-lists/src/fourth.rs
@@ -30,6 +30,35 @@ impl<T> List<T> {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+            }
+            None => self.tail = Some(new_head.clone()),
+        };
+
+        self.head = Some(new_head);
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    self.tail.take();
+                }
+            }
+
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
 }
 
 impl<T> Default for List<T> {
@@ -38,5 +67,32 @@ impl<T> Default for List<T> {
             head: None,
             tail: None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop_front(), None);
+
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        list.push_front(4);
+        list.push_front(5);
+
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
     }
 }
