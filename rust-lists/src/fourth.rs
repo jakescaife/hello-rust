92ab1b9919903a6f9b3241273b41e3095b332fc9
Jake Scaife <jakescaife@pm.me>
2020-06-09 07:16:21 +0100

    Implement operations for reverse side of deque

--- rust-lists/src/fourth.rs
+++ rust-lists/src/fourth.rs
@@ -39,7 +39,7 @@ impl<T> List<T> {
                 new_head.borrow_mut().next = Some(old_head);
             }
             None => self.tail = Some(new_head.clone()),
-        };
+        }
 
         self.head = Some(new_head);
     }
@@ -68,6 +68,48 @@ impl<T> List<T> {
 
     pub fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
         self.head
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
+    }
+
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+            }
+        }
+
+        self.tail = Some(new_tail);
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
+
+    pub fn peek_back(&self) -> Option<Ref<T>> {
+        self.tail
+            .as_ref()
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
+    }
+
+    pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
+        self.tail
             .as_ref()
             .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
@@ -115,6 +157,28 @@ mod tests {
     }
 
     #[test]
+    fn basics_back() {
+        let mut list = List::new();
+
+        assert!(list.pop_back().is_none());
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        list.push_back(4);
+        list.push_back(5);
+
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+        assert_eq!(list.pop_back(), Some(1));
+        assert!(list.pop_back().is_none());
+    }
+
+    #[test]
     fn peek() {
         let mut list = List::new();
         assert!(list.peek_front().is_none());
@@ -125,6 +189,20 @@ mod tests {
         list.push_front(3);
 
         assert_eq!(&*list.peek_front().unwrap(), &3);
-        assert_eq!(&*list.peek_front_mut().unwrap(), &mut 3);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+    }
+
+    #[test]
+    fn peek_back() {
+        let mut list = List::new();
+        assert!(list.peek_back().is_none());
+        assert!(list.peek_back_mut().is_none());
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(&*list.peek_back().unwrap(), &3);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 3);
     }
 }
